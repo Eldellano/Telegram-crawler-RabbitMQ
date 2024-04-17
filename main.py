da1a8209436ee7e62cb4e8699e57225efb4fbe9b
Vladislav Kalman <Vladislav99355@gmail.com>
2024-04-17 13:27:10 +0300

    added get_messages_from_date endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime, timedelta
+
 import uvicorn
 import json
 from fastapi import FastAPI, Request
@@ -33,6 +35,27 @@ async def get_all_messages(channel_name: str):
         worker.send_task(body_task)
 
     return channel_name
+
+
+@app.get('/get_messages_from_date/')
+async def get_messages_from_date(channel_name: str, date_from: str, date_to: str):
+    worker = Worker()
+
+    date_from = int(datetime.timestamp(datetime.strptime(date_from, '%d.%m.%Y')))
+
+    date_to = datetime.strptime(date_to, '%d.%m.%Y') + timedelta(days=1)  # для получения сообщений "включительно"
+    date_to = int(datetime.timestamp(date_to))
+
+    if channel_name:
+        print(channel_name, date_from, date_to)
+
+        task_params = {'channel_name': channel_name,
+                       'date_from': date_from,
+                       'date_to': date_to}
+        body_task = json.dumps(task_params).encode('utf-8')
+        worker.send_task(body_task)
+
+    return f'{channel_name=}, {date_from=}, {date_to=}'
 
 
 if __name__ == "__main__":
